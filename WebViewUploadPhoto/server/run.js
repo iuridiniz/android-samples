655f7aeae64bcbd7b92dc10ee0218fd10d65746f
Iuri Diniz <iuridiniz@gmail.com>
2012-07-15 16:00:49 -0300

    use formidable

--- WebViewUploadPhoto/server/run.js
+++ WebViewUploadPhoto/server/run.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 
+var formidable = require('formidable');
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+var util = require('util');
+
+http.createServer(function(req, res) {
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+    return;
+  }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
 }).listen(9002, '0.0.0.0');
+
 console.log('Server running at http://127.0.0.1:9002/');
 
 
